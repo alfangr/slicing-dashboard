6e718eb5bc86a31b24a7eb905b84bcb65da627b3
Alfan Ghinan Rusydi <alfangr05@gmail.com>
2021-06-24 00:51:23 +0700

    add some assets and update navbar menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brand: {
+          100: '#f8f9fe',
+          200: '#e7f6f5',
+          300: '#d4eef0',
+          400: '#27b7ad',
+          500: '#00aa9f',
+        }
+      }
+    }
   },
   variants: {
     extend: {}
